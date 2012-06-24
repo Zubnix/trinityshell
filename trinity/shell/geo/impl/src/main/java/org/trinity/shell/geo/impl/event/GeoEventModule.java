0e8d53c3e62df7b9a25ced0a24e938614f85bf93
Erik De Rijcke <DeRijcke.Erik@Gmail.com>
2012-06-24 10:12:25 -0700

    asdf

---
+++ trinity/shell/geo/impl/src/main/java/org/trinity/shell/geo/impl/event/GeoEventModule.java
@@ -0,0 +1,13 @@
+package org.trinity.shell.geo.impl.event;
+
+import com.google.inject.AbstractModule;
+
+public class GeoEventModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		// TODO
+
+	}
+
+}
