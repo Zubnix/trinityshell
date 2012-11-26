98edf68dd4ae382e61fbcceaf63e735dabe9696b
Erik De Rijcke <DeRijcke.Erik@Gmail.com>
2012-11-26 22:10:25 +0100

    typo fix

--- trinity/shell/core/api/src/main/java/org/trinity/shell/api/node/event/package-info.java
+++ trinity/shell/core/api/src/main/java/org/trinity/shell/api/node/event/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Events emitted by a {@link org.trinity.shell.api.node.event.ShellNodeEvent}.
+ * Events emitted by a {@link org.trinity.shell.api.node.ShellNode}.
  */
 package org.trinity.shell.api.node.event;
