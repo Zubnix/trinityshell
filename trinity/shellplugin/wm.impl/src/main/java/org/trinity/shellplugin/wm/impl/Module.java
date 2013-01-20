cd0eac89976bd94a9bf61a9f5e9e979bc8297f3a
Erik De Rijcke <DeRijcke.Erik@gmail.com>
2013-01-20 21:17:26 +0100

    removed all wm plugin files and added placeholder module class

---
+++ trinity/shellplugin/wm.impl/src/main/java/org/trinity/shellplugin/wm/impl/Module.java
@@ -0,0 +1,16 @@
+package org.trinity.shellplugin.wm.impl;
+
+import com.google.inject.AbstractModule;
+
+import de.devsurf.injection.guice.annotations.GuiceModule;
+
+@GuiceModule
+public class Module extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
