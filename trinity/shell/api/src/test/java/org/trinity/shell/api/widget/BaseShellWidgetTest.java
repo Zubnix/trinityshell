bd2133ac3ee590edef7bc28fc3279ad65e6180f0
Erik De Rijcke <DeRijcke.Erik@gmail.com>
2013-01-22 17:09:47 +0100

    added baseshellwidget initialization test

---
+++ trinity/shell/api/src/test/java/org/trinity/shell/api/widget/BaseShellWidgetTest.java
@@ -0,0 +1,55 @@
+package org.trinity.shell.api.widget;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.trinity.foundation.api.display.DisplaySurface;
+import org.trinity.foundation.api.render.Painter;
+import org.trinity.foundation.api.render.PainterFactory;
+import org.trinity.shell.api.surface.ShellDisplayEventDispatcher;
+
+import com.google.common.base.Optional;
+import com.google.common.eventbus.EventBus;
+
+public class BaseShellWidgetTest {
+
+	@Test
+	public void testInit() {
+		// given
+		final EventBus eventBus = mock(EventBus.class);
+		final ShellDisplayEventDispatcher shellDisplayEventDispatcher = mock(ShellDisplayEventDispatcher.class);
+		final PainterFactory painterFactory = mock(PainterFactory.class);
+		final ShellWidget parentWidget = mock(ShellWidget.class);
+		final DisplaySurface displaySurface = mock(DisplaySurface.class);
+		when(parentWidget.getDisplaySurface()).thenReturn(displaySurface);
+		final Painter painter = mock(Painter.class);
+		when(painterFactory.createPainter(any())).thenReturn(painter);
+		final BaseShellWidget baseShellWidget = new BaseShellWidget(eventBus,
+																	shellDisplayEventDispatcher,
+																	painterFactory);
+
+		when(painter.getDislaySurface()).thenReturn(Optional.of(displaySurface));
+
+		baseShellWidget.setX(50);
+		baseShellWidget.setY(75);
+		baseShellWidget.setWidth(100);
+		baseShellWidget.setHeight(200);
+
+		// when
+		baseShellWidget.setParent(parentWidget);
+		baseShellWidget.doReparent();
+
+		// then
+		verify(	painter,
+				times(1)).move(	50,
+								75);
+		verify(	painter,
+				times(1)).resize(	100,
+									200);
+
+	}
+}
