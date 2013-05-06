9dac2d4b8e5ee16e5e3fb96c380ef7dd5e10174d
Zubnix <DeRijcke.Erik@Gmail.com>
2013-05-06 16:41:38 +0200

    more send wm_delete icccm impl wip

---
+++ shellplugin/wm.x11.impl/src/main/java/org/trinity/shellplugin/wm/x11/impl/protocol/XClientMessageSender.java
@@ -0,0 +1,67 @@
+package org.trinity.shellplugin.wm.x11.impl.protocol;
+
+import java.nio.ByteBuffer;
+
+import org.freedesktop.xcb.xcb_client_message_data_t;
+import org.freedesktop.xcb.xcb_client_message_event_t;
+import org.freedesktop.xcb.xcb_generic_event_t;
+import org.trinity.foundation.api.display.DisplaySurface;
+import org.trinity.shellplugin.wm.x11.impl.XConnection;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import de.devsurf.injection.guice.annotations.Bind;
+import de.devsurf.injection.guice.annotations.To;
+import de.devsurf.injection.guice.annotations.To.Type;
+import static org.freedesktop.xcb.LibXcb.xcb_send_event;
+
+@Bind(to = @To(value = Type.IMPLEMENTATION))
+@Singleton
+public class XClientMessageSender {
+	private final XConnection xConnection;
+
+	public enum Format {
+		Byte((short) 8), Short((short) 16), Integer((short) 32);
+
+		private final short format;
+
+		private Format(final short format) {
+			this.format = format;
+
+		}
+
+		public short getFormat() {
+			return this.format;
+		}
+	}
+
+	@Inject
+	XClientMessageSender(final XConnection xConnection) {
+		this.xConnection = xConnection;
+	}
+
+	public void sendMessage(final DisplaySurface xWindow,
+							final int type,
+							final Format format,
+							final ByteBuffer message) {
+		final Integer winId = (Integer) xWindow.getDisplaySurfaceHandle().getNativeHandle();
+		final short messageFormat = format.getFormat();
+		final xcb_client_message_data_t message_data = new xcb_client_message_data_t();
+		final int[] messageData = new int[5];
+		message.asIntBuffer().get(messageData);
+		message_data.setData32(messageData);
+
+		final xcb_client_message_event_t client_message_event = new xcb_client_message_event_t();
+		client_message_event.setFormat(messageFormat);
+		client_message_event.setData(message_data);
+
+		final xcb_generic_event_t generic_event = new xcb_generic_event_t(	xcb_client_message_event_t.getCPtr(client_message_event),
+																			true);
+		xcb_send_event(	this.xConnection.getConnectionRef(),
+						(short) 0,
+						winId.intValue(),
+						0,
+						generic_event);
+	}
+}
--- shellplugin/wm.x11.impl/src/main/java/org/trinity/shellplugin/wm/x11/impl/scene/ClientTopBarItem.java
+++ shellplugin/wm.x11.impl/src/main/java/org/trinity/shellplugin/wm/x11/impl/scene/ClientTopBarItem.java
@@ -1,9 +1,5 @@
 package org.trinity.shellplugin.wm.x11.impl.scene;
 
-import static com.google.common.util.concurrent.Futures.addCallback;
-import static org.freedesktop.xcb.LibXcb.xcb_icccm_get_wm_name;
-import static org.freedesktop.xcb.LibXcb.xcb_icccm_get_wm_name_reply;
-
 import java.util.concurrent.Callable;
 
 import org.freedesktop.xcb.xcb_generic_error_t;
@@ -13,6 +9,7 @@ import org.freedesktop.xcb.xcb_property_notify_event_t;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.trinity.foundation.api.display.DisplaySurface;
+import org.trinity.foundation.api.display.input.Momentum;
 import org.trinity.foundation.api.display.input.PointerInput;
 import org.trinity.foundation.api.render.binding.model.PropertyChanged;
 import org.trinity.shell.api.surface.ShellSurface;
@@ -20,6 +17,7 @@ import org.trinity.shellplugin.wm.api.HasText;
 import org.trinity.shellplugin.wm.api.ReceivesPointerInput;
 import org.trinity.shellplugin.wm.x11.impl.XConnection;
 import org.trinity.shellplugin.wm.x11.impl.protocol.XAtomCache;
+import org.trinity.shellplugin.wm.x11.impl.protocol.XClientMessageSender;
 
 import com.google.common.eventbus.Subscribe;
 import com.google.common.util.concurrent.FutureCallback;
@@ -28,6 +26,11 @@ import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
 import com.google.inject.name.Named;
 
+import static org.freedesktop.xcb.LibXcb.xcb_icccm_get_wm_name;
+import static org.freedesktop.xcb.LibXcb.xcb_icccm_get_wm_name_reply;
+
+import static com.google.common.util.concurrent.Futures.addCallback;
+
 public class ClientTopBarItem implements HasText, ReceivesPointerInput {
 
 	private static Logger logger = LoggerFactory.getLogger(ClientTopBarItem.class);
@@ -35,21 +38,27 @@ public class ClientTopBarItem implements HasText, ReceivesPointerInput {
 	private final ShellSurface client;
 	private final XAtomCache xAtomCache;
 	private final ListeningExecutorService wmExecutor;
+	private final XClientMessageSender xClientMessageSender;
 	private final XConnection xConnection;
 	private String clientName = "";
 
 	private final ReceivesPointerInput closeButton = new ReceivesPointerInput() {
+		// called by shell thread.
 		@Override
 		public void onPointerInput(final PointerInput pointerInput) {
-			sendClientCloseRequest();
+			if (pointerInput.getMomentum() == Momentum.STOPPED) {
+				handleClientCloseRequest();
+			}
 		}
 	};
 
 	@AssistedInject
 	ClientTopBarItem(	@Named("WindowManager") final ListeningExecutorService wmExecutor,
+						final XClientMessageSender xClientMessageSender,
 						final XAtomCache xAtomCache,
 						final XConnection xConnection,
 						@Assisted final ShellSurface client) {
+		this.xClientMessageSender = xClientMessageSender;
 		this.client = client;
 		this.xAtomCache = xAtomCache;
 		this.xConnection = xConnection;
@@ -59,6 +68,7 @@ public class ClientTopBarItem implements HasText, ReceivesPointerInput {
 						@Override
 						public void onSuccess(final DisplaySurface clientXWindow) {
 							clientXWindow.register(new Object() {
+								// called by window manager thread
 								@Subscribe
 								public void onXPropertyChanged(final xcb_property_notify_event_t notify_event_t) {
 									updateClientName(clientXWindow);
@@ -76,6 +86,7 @@ public class ClientTopBarItem implements HasText, ReceivesPointerInput {
 					wmExecutor);
 	}
 
+	// called by window manager thread
 	public void updateClientName(final DisplaySurface clientXWindow) {
 		final int window = (Integer) clientXWindow.getDisplaySurfaceHandle().getNativeHandle();
 		final xcb_get_property_cookie_t get_property_cookie_t = xcb_icccm_get_wm_name(	this.xConnection.getConnectionRef(),
@@ -97,6 +108,7 @@ public class ClientTopBarItem implements HasText, ReceivesPointerInput {
 					return null;
 				}
 
+				// TODO better text parsing.
 				setText(prop.getName());
 
 				return null;
@@ -108,22 +120,29 @@ public class ClientTopBarItem implements HasText, ReceivesPointerInput {
 		return this.closeButton;
 	}
 
-	private void sendClientCloseRequest() {
+	// called by shell thread.
+	private void handleClientCloseRequest() {
 		addCallback(this.client.getDisplaySurface(),
 					new FutureCallback<DisplaySurface>() {
 						@Override
 						public void onSuccess(final DisplaySurface clientXWindow) {
-							// TODO Auto-generated method stub
-
+							sendWmDeleteMessage(clientXWindow);
 						}
 
 						@Override
 						public void onFailure(final Throwable t) {
-							// TODO Auto-generated method stub
-
+							logger.error(	"Error getting display surface from client.",
+											t);
 						}
 					},
 					this.wmExecutor);
+	}
+
+	private void sendWmDeleteMessage(final DisplaySurface clientXWindow) {
+		// this.xClientMessageSender.sendMessage( clientXWindow,
+		// type,
+		// Format.Integer,
+		// message);
 	}
 
 	@PropertyChanged("text")
@@ -137,8 +156,25 @@ public class ClientTopBarItem implements HasText, ReceivesPointerInput {
 		return this.clientName;
 	}
 
+	// called by shell thread.
 	@Override
 	public void onPointerInput(final PointerInput pointerInput) {
-		// do something with client, eg give focus?
+		addCallback(this.client.getDisplaySurface(),
+					new FutureCallback<DisplaySurface>() {
+						@Override
+						public void onSuccess(final DisplaySurface clientXWindow) {
+							clientXWindow.setInputFocus();
+							ClientTopBarItem.this.client.doRaise();
+						}
+
+						@Override
+						public void onFailure(final Throwable t) {
+							logger.error(	"Error getting display surface from client.",
+											t);
+						}
+					},
+					this.wmExecutor);
+
+		// do something with client, eg raise & give focus?
 	}
 }
