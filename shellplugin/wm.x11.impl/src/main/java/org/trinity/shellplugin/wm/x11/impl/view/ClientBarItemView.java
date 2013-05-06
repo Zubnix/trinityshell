324e5c745577cdcbd1d1c772cb471aba85e05644
Zubnix <DeRijcke.Erik@Gmail.com>
2013-05-06 15:39:25 +0200

    added basic layout to clientbaritem

--- shellplugin/wm.x11.impl/src/main/java/org/trinity/shellplugin/wm/x11/impl/view/ClientBarItemView.java
+++ shellplugin/wm.x11.impl/src/main/java/org/trinity/shellplugin/wm/x11/impl/view/ClientBarItemView.java
@@ -10,50 +10,82 @@ import org.trinity.foundation.api.shared.Size;
 import org.trinity.shellplugin.wm.api.ImageDefinition;
 
 import com.google.common.base.Preconditions;
+import com.trolltech.qt.core.QMargins;
 import com.trolltech.qt.gui.QFrame;
+import com.trolltech.qt.gui.QHBoxLayout;
 import com.trolltech.qt.gui.QLabel;
 import com.trolltech.qt.gui.QPixmap;
 
-@PropertySlots({ //
-@PropertySlot(propertyName = "text", methodName = "setText", argumentTypes = { String.class }),// HasText
-		@PropertySlot(propertyName = "imageDefinition", methodName = "setImageDefinition", argumentTypes = { ImageDefinition.class }) // HasImageDefinition
-})
-@InputSignals({ //
-@InputSignal(name = "onPointerInput", inputType = PointerInput.class) // ReceivesPointerInput
-})
 class ClientBarItemView extends QLabel {
 
-	{
-		// workaround for jambi css bug
-		setObjectName(getClass().getSimpleName());
-	}
+	@PropertySlots({ //
+	@PropertySlot(propertyName = "imageDefinition", methodName = "setImageDefinition", argumentTypes = { ImageDefinition.class }) // HasImageDefinition
+	})
+	@InputSignals({ //
+	@InputSignal(name = "onPointerInput", inputType = PointerInput.class) // ReceivesPointerInput
+	})
+	QLabel clientIconView = new QLabel() {
+		{
+			// workaround for jambi css bug
+			setObjectName("clientIconView");
+		}
+
+		void setImageDefinition(final ImageDefinition imageDefinition) {
+			final Size imageSize = imageDefinition.getImageSize();
+			final int width = imageSize.getWidth();
+			final int height = imageSize.getHeight();
+			Preconditions.checkState(	width > 0,
+										"Image width must be > 0");
+			Preconditions.checkState(	height > 0,
+										"Image height must be > 0");
+
+			final byte[] imageData = imageDefinition.getImageData();
+
+			final QPixmap pixmapImage = new QPixmap(width,
+													height);
+			pixmapImage.loadFromData(imageData);
+			setPixmap(pixmapImage);
+		}
+	};
+
+	@PropertySlots({ //
+	@PropertySlot(propertyName = "text", methodName = "setText", argumentTypes = { String.class }) // HasText
+	})
+	@InputSignals({ //
+	@InputSignal(name = "onPointerInput", inputType = PointerInput.class) // ReceivesPointerInput
+	})
+	QLabel clientNameView = new QLabel() {
+		{
+			// workaround for jambi css bug
+			setObjectName("clientNameView");
+		}
+	};
 
 	@DataContext("closeButton")
 	@InputSignals({ //
 	@InputSignal(name = "onPointerInput", inputType = PointerInput.class) // ReceivesPointerInput
 	})
-	QFrame closeButtonView = new QFrame(this) {
+	QFrame closeButtonView = new QFrame() {
 		// workaround for jambi css bug
 		{
 			setObjectName("CloseButtonView");
 		}
 	};
 
-	public void setImageDefinition(final ImageDefinition imageDefinition) {
-		final Size imageSize = imageDefinition.getImageSize();
-		final int width = imageSize.getWidth();
-		final int height = imageSize.getHeight();
-		Preconditions.checkState(	width > 0,
-									"Image width must be > 0");
-		Preconditions.checkState(	height > 0,
-									"Image height must be > 0");
+	QHBoxLayout barItemlayout = new QHBoxLayout(this);
 
-		final byte[] imageData = imageDefinition.getImageData();
+	{
+		// workaround for jambi css bug
+		setObjectName(getClass().getSimpleName());
 
-		final QPixmap pixmapImage = new QPixmap(width,
-												height);
-		pixmapImage.loadFromData(imageData);
-		setPixmap(pixmapImage);
+		this.barItemlayout.setContentsMargins(new QMargins(	0,
+															0,
+															0,
+															0));
+		setLayout(this.barItemlayout);
+
+		this.barItemlayout.addWidget(this.clientIconView);
+		this.barItemlayout.addWidget(this.clientNameView);
+		this.barItemlayout.addWidget(this.closeButtonView);
 	}
-
 }
