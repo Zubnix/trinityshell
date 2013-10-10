e94d576bcc97a5ce8dcea3fc1b8a005917ae94fc
Erik De Rijcke <derijcke.erik@gmail.com>
2013-10-10 22:52:49 +0200

    fix concurrency issue with async eventbus where listeners were added too late

--- foundation/api/src/main/java/org/trinity/foundation/api/shared/AsyncListenableEventBus.java
+++ foundation/api/src/main/java/org/trinity/foundation/api/shared/AsyncListenableEventBus.java
@@ -20,17 +20,20 @@
 
 package org.trinity.foundation.api.shared;
 
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
-
-import javax.annotation.Nonnull;
-import javax.annotation.concurrent.ThreadSafe;
-
-import com.google.common.eventbus.AsyncEventBus;
-import com.google.common.eventbus.EventBus;
-import com.google.common.util.concurrent.MoreExecutors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Extends Guava's {@link EventBus} with asynchronous event delivery per listener.
@@ -39,15 +42,17 @@ import com.google.common.util.concurrent.MoreExecutors;
 public class AsyncListenableEventBus extends EventBus implements AsyncListenable {
 
 	private final Map<Object, AsyncEventBus> asyncEventBusByListener = new WeakHashMap<Object, AsyncEventBus>();
-	private final ExecutorService listenableExecutorService;
+    Lock scheduledListenersLock = new ReentrantLock(true);
+    private Deque<Callable> scheduledListeners = new LinkedList<>();
+    private final ExecutorService listenableExecutorService;
 
-	public AsyncListenableEventBus(@Nonnull final ExecutorService postingExecutorService) {
-		this.listenableExecutorService = postingExecutorService;
-	}
+    public AsyncListenableEventBus(@Nonnull final ExecutorService postingExecutorService) {
+        this.listenableExecutorService = postingExecutorService;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * <p>
+    /**
+     * {@inheritDoc}
+     * <p>
 	 *
 	 * @see EventBus#register(Object)
 	 */
@@ -60,24 +65,31 @@ public class AsyncListenableEventBus extends EventBus implements AsyncListenable
 	@Override
 	public void register(	@Nonnull final Object object,
 							@Nonnull final ExecutorService executor) {
-		this.listenableExecutorService.submit(new Callable<Void>() {
-			@Override
-			public Void call() {
-				final AsyncEventBus asyncEventBus = new AsyncEventBus(executor);
-				asyncEventBus.register(object);
-				AsyncListenableEventBus.this.asyncEventBusByListener.put(	object,
-																			asyncEventBus);
-				return null;
-			}
-		});
-	}
+        //schedule the listener for inclusion when the next event notification starts.
+        Callable<Void> scheduledListener = new Callable<Void>() {
+            @Override
+            public Void call() {
+                final AsyncEventBus asyncEventBus = new AsyncEventBus(executor);
+                asyncEventBus.register(object);
+                AsyncListenableEventBus.this.asyncEventBusByListener.put(object,
+                                                                         asyncEventBus);
+                return null;
+            }
+        };
+        scheduledListenersLock.lock();
+        try {
+            scheduledListeners.add(scheduledListener);
+        } finally {
+            scheduledListenersLock.unlock();
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * <p>
-	 *
-	 * @see EventBus#unregister(Object)
-	 */
+    /**
+     * {@inheritDoc}
+     * <p>
+     *
+     * @see EventBus#unregister(Object)
+     */
 	@Override
 	public void unregister(@Nonnull final Object object) {
 		this.listenableExecutorService.submit(new Callable<Void>() {
@@ -99,13 +111,26 @@ public class AsyncListenableEventBus extends EventBus implements AsyncListenable
 	public void post(@Nonnull final Object event) {
 		this.listenableExecutorService.submit(new Callable<Void>() {
 			@Override
-			public Void call() {
-				for (final AsyncEventBus asyncEventBus : AsyncListenableEventBus.this.asyncEventBusByListener.values()) {
-					asyncEventBus.post(event);
-				}
-				return null;
-			}
-		});
-	}
+            public Void call() throws Exception {
+                addScheduledListeners();
+
+                for(final AsyncEventBus asyncEventBus : AsyncListenableEventBus.this.asyncEventBusByListener.values()) {
+                    asyncEventBus.post(event);
+                }
+                return null;
+            }
+        });
+    }
+
+    private void addScheduledListeners() throws Exception {
+        scheduledListenersLock.lock();
+        try {
+            for(Callable<Void> scheduledListener : scheduledListeners) {
+                scheduledListener.call();
+            }
+        } finally {
+            scheduledListenersLock.unlock();
+        }
+    }
 
 }
