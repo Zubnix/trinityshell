4edc503154e633eabd1c32c48b26c7950a58d13a
Erik De Rijcke <derijcke.erik@gmail.com>
2013-10-27 20:28:31 +0100

    Finished XScreenImpl tests

---
+++ foundation/display.x11.impl/src/test/java/org/trinity/foundation/display/x11/impl/TestXScreenImpl.java
@@ -0,0 +1,38 @@
+package org.trinity.foundation.display.x11.impl;
+
+import org.freedesktop.xcb.xcb_screen_t;
+import org.junit.Test;
+import org.trinity.foundation.api.shared.Size;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class TestXScreenImpl {
+
+    @Test
+    public void testScreenSizeHappy() {
+        //given
+        //an X screen with a given width and height in pixels
+        xcb_screen_t xcb_screen = mock(xcb_screen_t.class);
+
+        int width = 100;
+        int height = 100;
+        when(xcb_screen.getWidth_in_pixels()).thenReturn(width);
+        when(xcb_screen.getHeight_in_pixels()).thenReturn(height);
+
+        //when
+        //the screen pixel size is requested
+        XScreenImpl xScreen = new XScreenImpl(xcb_screen);
+        Size size = xScreen.getSize();
+
+        //then
+        //the correct pixel size is reported
+        verify(xcb_screen).getWidth_in_pixels();
+        verify(xcb_screen).getHeight_in_pixels();
+
+        assertEquals(width,
+                     size.getWidth());
+        assertEquals(height,
+                     size.getHeight());
+    }
+}
