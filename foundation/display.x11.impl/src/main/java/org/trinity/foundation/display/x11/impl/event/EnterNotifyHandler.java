edb84629392e8dd39dc48e9b1f9ebb5fbc5bbf1b
Erik De Rijcke <DeRijcke.Erik@Gmail.com>
2013-11-14 14:28:20 +0100

    Finished entery notify handler tests

--- foundation/display.x11.impl/src/main/java/org/trinity/foundation/display/x11/impl/event/EnterNotifyHandler.java
+++ foundation/display.x11.impl/src/main/java/org/trinity/foundation/display/x11/impl/event/EnterNotifyHandler.java
@@ -19,10 +19,10 @@
  ******************************************************************************/
 package org.trinity.foundation.display.x11.impl.event;
 
-import static org.freedesktop.xcb.LibXcbConstants.XCB_ENTER_NOTIFY;
-
-import javax.annotation.concurrent.Immutable;
-
+import com.google.common.base.Optional;
+import com.google.common.eventbus.EventBus;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import org.apache.onami.autobind.annotations.Bind;
 import org.freedesktop.xcb.xcb_enter_notify_event_t;
 import org.freedesktop.xcb.xcb_generic_event_t;
@@ -30,17 +30,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.trinity.foundation.api.display.DisplaySurface;
 import org.trinity.foundation.api.display.bindkey.DisplayExecutor;
-import org.trinity.foundation.api.display.event.PointerLeaveNotify;
+import org.trinity.foundation.api.display.event.PointerEnterNotify;
 import org.trinity.foundation.api.shared.ExecutionContext;
 import org.trinity.foundation.display.x11.api.XEventHandler;
-import org.trinity.foundation.display.x11.api.bindkey.XEventBus;
 import org.trinity.foundation.display.x11.api.XWindowHandle;
+import org.trinity.foundation.display.x11.api.bindkey.XEventBus;
 import org.trinity.foundation.display.x11.impl.XWindowPoolImpl;
 
-import com.google.common.base.Optional;
-import com.google.common.eventbus.EventBus;
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import static org.freedesktop.xcb.LibXcbConstants.XCB_ENTER_NOTIFY;
 
 @Bind(multiple = true)
 @Singleton
@@ -51,17 +51,17 @@ public class EnterNotifyHandler implements XEventHandler {
 	private static final Logger LOG = LoggerFactory.getLogger(EnterNotifyHandler.class);
 	private static final Integer EVENT_CODE = XCB_ENTER_NOTIFY;
 	private final EventBus xEventBus;
-	private final XWindowPoolImpl xWindowCache;
+	private final XWindowPoolImpl xWindowPool;
 
 	@Inject
 	EnterNotifyHandler(	@XEventBus final EventBus xEventBus,
-						final XWindowPoolImpl xWindowCache) {
+						final XWindowPoolImpl xWindowPool) {
 		this.xEventBus = xEventBus;
-		this.xWindowCache = xWindowCache;
+		this.xWindowPool = xWindowPool;
 	}
 
 	@Override
-	public Optional<PointerLeaveNotify> handle(final xcb_generic_event_t event_t) {
+	public Optional<PointerEnterNotify> handle(@Nonnull final xcb_generic_event_t event_t) {
 		final xcb_enter_notify_event_t enter_notify_event = cast(event_t);
 
 		LOG.debug(	"Received X event={}",
@@ -69,7 +69,7 @@ public class EnterNotifyHandler implements XEventHandler {
 
 		this.xEventBus.post(enter_notify_event);
 
-		return Optional.of(new PointerLeaveNotify());
+		return Optional.of(new PointerEnterNotify());
 	}
 
 	private xcb_enter_notify_event_t cast(final xcb_generic_event_t event_t) {
@@ -78,10 +78,10 @@ public class EnterNotifyHandler implements XEventHandler {
 	}
 
 	@Override
-	public Optional<DisplaySurface> getTarget(final xcb_generic_event_t event_t) {
+	public Optional<DisplaySurface> getTarget(@Nonnull final xcb_generic_event_t event_t) {
 		final xcb_enter_notify_event_t enter_notify_event_t = cast(event_t);
 		final int windowId = enter_notify_event_t.getEvent();
-		return Optional.of(this.xWindowCache.getDisplaySurface(new XWindowHandle(windowId)));
+		return Optional.of(this.xWindowPool.getDisplaySurface(new XWindowHandle(windowId)));
 	}
 
 	@Override
