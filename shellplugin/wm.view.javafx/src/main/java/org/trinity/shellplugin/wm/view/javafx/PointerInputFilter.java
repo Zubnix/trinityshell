5c3f9c5b2bf0b5658bbb3d9219f1490ebc10f525
Erik De Rijcke <DeRijcke.Erik@Gmail.com>
2013-12-17 10:13:47 +0100

    implemented removing of point input filter

--- shellplugin/wm.view.javafx/src/main/java/org/trinity/shellplugin/wm/view/javafx/PointerInputFilter.java
+++ shellplugin/wm.view.javafx/src/main/java/org/trinity/shellplugin/wm/view/javafx/PointerInputFilter.java
@@ -1,5 +1,7 @@
 package org.trinity.shellplugin.wm.view.javafx;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
@@ -11,15 +13,33 @@ import javax.annotation.Nonnull;
 import static javafx.scene.input.MouseEvent.MOUSE_CLICKED;
 
 public class PointerInputFilter implements EventSignalFilter {
+
+    private final Table<Object,Signal,EventHandler<MouseEvent>> eventHandlerTable = HashBasedTable.create();
+
     @Override
     public void installFilter(@Nonnull final Object view,
                               @Nonnull final Signal signal) {
-        Node node = (Node) view;
+
+        final EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+            public void handle(final MouseEvent event) {
+                signal.fire();
+            }
+        };
+        this.eventHandlerTable.put(view,signal,eventHandler);
+
+        final Node node = (Node) view;
         node.addEventHandler(MOUSE_CLICKED,
-                             new EventHandler<MouseEvent>() {
-                                 public void handle(MouseEvent event) {
-                                     signal.fire();
-                                 }
-                             });
+                             eventHandler);
+    }
+
+    @Override
+    public void uninstallFilter(@Nonnull final Object view,
+                                @Nonnull final Signal signal) {
+
+        final EventHandler<MouseEvent> eventHandler = this.eventHandlerTable.get(view, signal);
+
+        final Node node = (Node) view;
+        node.removeEventHandler(MOUSE_CLICKED,
+                                eventHandler);
     }
 }
