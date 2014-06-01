d3a9aa88adc02c7e5d5dea9badd671975c26138c
Zubnix <derijcke.erik@gmail.com>
2014-06-01 21:54:46 +0200

    implement wel event loop event handling

--- shell.default/src/main/java/org/trinity/wayland/defaul/WlShellService.java
+++ shell.default/src/main/java/org/trinity/wayland/defaul/WlShellService.java
@@ -1,52 +1,48 @@
 package org.trinity.wayland.defaul;
 
+import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.AbstractIdleService;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.ListeningExecutorService;
 import jnr.constants.platform.Fcntl;
 import jnr.ffi.LastError;
 import jnr.ffi.Runtime;
 import org.freedesktop.wayland.server.Display;
 import org.freedesktop.wayland.server.EventLoop;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.trinity.LibC;
 
-import javax.annotation.Nullable;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.concurrent.Callable;
-
-import static com.google.common.util.concurrent.Futures.addCallback;
-import static com.google.common.util.concurrent.MoreExecutors.listeningDecorator;
-import static java.util.concurrent.Executors.newSingleThreadExecutor;
+import java.util.LinkedList;
 
 /**
  * Created by Erik De Rijcke on 5/26/14.
  */
-public class WlShellService extends AbstractIdleService {
+public class WlShellService extends AbstractIdleService implements EventLoop.FileDescriptorEventHandler {
 
-    private static final int        FD_CLOEXEC         = 1;
-    private static final ByteBuffer EVENT_JOB_NEW      = ByteBuffer.allocateDirect(1)
-                                                                   .putInt(1);
-    private static final ByteBuffer EVENT_JOB_FINISHED = ByteBuffer.allocateDirect(1)
-                                                                   .putInt(0);
+    private static final Logger LOGGER = LoggerFactory.getLogger(WlShellService.class);
 
-    private final ListeningExecutorService executor = listeningDecorator(newSingleThreadExecutor());
+    private static final int                  FD_CLOEXEC          = 1;
+    private static final byte                 EVENT_NEW_JOB       = 1;
+    private final        ByteBuffer           newJobEventBuffer   = ByteBuffer.allocateDirect(1)
+                                                                              .putInt(EVENT_NEW_JOB);
+    private static final byte                 EVENT_FINISHED      = 0;
+    private final        ByteBuffer           finishedEventBuffer = ByteBuffer.allocateDirect(1)
+                                                                              .putInt(EVENT_FINISHED);
+    private final        ByteBuffer           eventReadBuffer     = ByteBuffer.allocateDirect(1);
+    private final        LinkedList<Runnable> jobs                = Lists.newLinkedList();
 
-    private final Display        display;
-    private final WlEventHandler wlEventHandler;
-    private final LibC           libC;
+    private final Display display;
+    private final LibC    libC;
 
     private EventLoop.EventSource eventSource;
     private int[]                 pipeFds;
 
     @Inject
     WlShellService(final Display display,
-                   final WlEventHandler wlEventHandler,
                    final LibC libC) {
         this.display = display;
-        this.wlEventHandler = wlEventHandler;
         this.libC = libC;
     }
 
@@ -56,7 +52,7 @@ public class WlShellService extends AbstractIdleService {
         this.eventSource = this.display.getEventLoop()
                                        .addFileDescriptor(pipeFds[0],
                                                           EventLoop.EVENT_READABLE,
-                                                          this.wlEventHandler);
+                                                          this);
     }
 
     private int[] createPipe() throws IOException {
@@ -71,7 +67,7 @@ public class WlShellService extends AbstractIdleService {
         return this.libC.strerror(LastError.getLastError(Runtime.getRuntime(this.libC)));
     }
 
-    private int[] configurePipe(int[] pipeFds) throws IOException {
+    private int[] configurePipe(final int[] pipeFds) throws IOException {
 
         final int readFd = pipeFds[0];
         final int writeFd = pipeFds[1];
@@ -105,51 +101,68 @@ public class WlShellService extends AbstractIdleService {
 
     @Override
     protected void shutDown() throws IOException {
-        if (this.libC.close(this.pipeFds[0]) == -1) {
+        if (this.libC.write(this.pipeFds[1],
+                            this.finishedEventBuffer,
+                            1) != 1) {
             throw new IOException(getLastErrorString());
         }
+    }
+
+    private void cleanUp() {
+        if (this.libC.close(this.pipeFds[0]) == -1) {
+            LOGGER.error("Failed to close pipe read fd",
+                         new IOException(getLastErrorString()));
+        }
         if (this.libC.close(this.pipeFds[1]) == -1) {
-            throw new IOException(getLastErrorString());
+            LOGGER.error("Failed to close pipe write fd",
+                         new IOException(getLastErrorString()));
         }
 
         this.display.getEventLoop()
                     .remove(this.eventSource);
     }
 
-    public <T> ListenableFuture<T> run(Callable<T> job) {
+    @Override
+    public int handleFileDescriptorEvent(final int fd,
+                                         final int mask) {
+        do {
+            this.eventReadBuffer.clear();
+            this.libC.read(this.pipeFds[0],
+                           this.eventReadBuffer,
+                           1);
+            this.eventReadBuffer.rewind();
+            handleEvent(this.eventReadBuffer.get());
+        } while (!this.jobs.isEmpty());
+
+        return 0;
+    }
+
+    private void handleEvent(final byte event) {
+        if (event == EVENT_FINISHED) {
+            cleanUp();
+        }
+        else if (event == EVENT_NEW_JOB) {
+            this.jobs.poll()
+                     .run();
+        }
+        else {
+            throw new IllegalStateException();
+        }
+    }
+
+    public void offer(Runnable job) {
         try {
-            jobStarted();
-            final ListenableFuture<T> future = this.executor.submit(job);
-            addCallback(future,
-                        new FutureCallback<T>() {
-                            @Override
-                            public void onSuccess(@Nullable T t) {
-                                jobFinished();
-                            }
-
-                            @Override
-                            public void onFailure(Throwable throwable) {
-
-                            }
-                        });
-            return future;
+            this.jobs.offer(job);
+            jobNew();
         } catch (IOException e) {
 
             e.printStackTrace();
         }
     }
 
-    private void jobStarted() throws IOException {
+    private void jobNew() throws IOException {
         if (this.libC.write(this.pipeFds[1],
-                            EVENT_JOB_NEW,
-                            1) != 1) {
-            throw new IOException(getLastErrorString());
-        }
-    }
-
-    private void jobFinished() throws IOException {
-        if (this.libC.write(this.pipeFds[1],
-                            EVENT_JOB_FINISHED,
+                            newJobEventBuffer,
                             1) != 1) {
             throw new IOException(getLastErrorString());
         }
